43320d1b746cc792c9fb74550124eb4cfecb9cfb
Myron <myronmax_seelen@live.nl>
2021-04-19 23:08:04 +0200

    Inc commit

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
 <header id="header" class="fixed-top ">
     <div class="container d-flex align-items-center">
 
-        <h1 class="logo me-auto"><a href="index.html">Learn IT</a></h1>
+        <h1 class="logo me-auto"><a href="index.php">Learn IT</a></h1>
 
         <nav id="navbar" class="navbar">
             <ul>
